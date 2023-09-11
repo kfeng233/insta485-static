bf02f61623f13190a67fe08244fe576f89f1fcd7
kfeng233 <kaixfeng@umich.edu>
2023-09-10 22:09:58 -0400

    Refined copy_dir function

--- insta485generator/__main__.py
+++ insta485generator/__main__.py
@@ -23,7 +23,7 @@ def read_config(input_dir):
     except json.JSONDecodeError as err:
         click.echo(f"Error: {config_file}")
         click.echo(f"Expecting value: line {err.lineno}\
-                   column {err.colno} (char {err.pos})")
+        column {err.colno} (char {err.pos})")
         return None
 
 
@@ -49,31 +49,33 @@ def fill_template(input_dir, config_object, template):
         return None
 
 
-def write_output(input_dir, output_dir, rendered_template,
-                 verbose, static_copied):
+def write_output(output_dir, rendered_template, config_obj, verbose):
     """Write a output HTML file and copy static directory."""
-    output_path = pathlib.Path(output_dir/"index.html")
-    if os.path.exists(output_path):
-        click.echo(f"Error: {output_path} already exists")
+    output_file = pathlib.Path(output_dir/"index.html")
+    if os.path.exists(output_file):
+        click.echo(f"Error: {output_file} already exists")
     else:
         os.makedirs(pathlib.Path(output_dir), exist_ok=True)
-        with open(output_path, 'w', encoding="utf-8") as file:
-            file.write(rendered_template)
-        # if static exists, copy its directory
-        if not static_copied:
-            static_copied = copy_dir(input_dir, output_dir, verbose)
-    return static_copied
+        try:
+            with open(output_file, 'w', encoding="utf-8") as file:
+                file.write(rendered_template)
+            if verbose:
+                click.echo(
+                    f"Rendered {config_obj['template']} -> {output_file}")
+        except FileNotFoundError:
+            click.echo(f"Error: {output_dir} not found")
 
 
 def copy_dir(input_dir, output, verbose):
     """Copy static directory to the input directory."""
     src_dir = os.path.join(input_dir, "static")
-    if os.path.exists(src_dir):
+    try:
         shutil.copytree(src_dir, output, dirs_exist_ok=True)
         # verbose option
         if verbose:
             click.echo(f"Copied {src_dir} -> {output}")
-    return True
+    except FileNotFoundError:
+        click.echo(f"Error: {src_dir} not found")
 
 
 @click.command()
@@ -86,22 +88,17 @@ def main(input_dir, output, verbose):
     config_objects = read_config(input_dir)
     if config_objects is None:
         return
-    static_copied = False
+    # non --output option -> use default
+    if output is None:
+        output = pathlib.Path(input_dir/"html")
+    copy_dir(input_dir, output, verbose)
     for obj in config_objects:
         rendered_template = fill_template(input_dir, obj, obj['template'])
         if rendered_template is None:
             return
-        # non --output option -> use default
-        if output is None:
-            output = pathlib.Path(input_dir/"html")
         url = pathlib.Path(obj['url'].lstrip("/"))
         output_dir = pathlib.Path(output/url)
-        output_path = pathlib.Path(output/url/"index.html")
-        static_copied = write_output(input_dir, output_dir,
-                                     rendered_template, verbose, static_copied)
-        if verbose:
-            click.echo(
-                f"Rendered {obj['template']} -> {output_path}")
+        write_output(output_dir, rendered_template, obj, verbose)
 
 
 if __name__ == "__main__":
